7c64f05d25041fb8e1610399e41b13d5fac4cadf
Nikolay Angelov <nikolay.angelov.angelov@gmail.com>
2015-03-08 22:29:49 +0200

    Pass parameters in order to load dynamic content

--- DynamicTemplates/Scripts/articles.js
+++ DynamicTemplates/Scripts/articles.js
@@ -2,8 +2,10 @@
 
 function add() {
     var url = $('#article-list').data('request-url');
-    var jqxhr = $.get(url, function (data) {
-        console.log(data);
+    var title = $("#title").val();
+    var description = $("#description").val();
+    var jqxhr = $.get(url, { title: title, description: description }, function (data) {
+        $('#article-list li:eq(0)').before("<li>" + data + "</li>");
     })
     .fail(function () {
         alert("Error: cannot load the data.");
